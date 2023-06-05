58e2147a341e633b2f62a4ceab474be260b9b7c9
Jenny Boucher <jebouche@c2r5p4.hive.fi>
2023-06-05 09:35:42 +0300

    remove commented lines and function to intialize pointers to functions

--- srcs/setup.c
+++ srcs/setup.c
@@ -6,20 +6,11 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 18:41:11 by jebouche          #+#    #+#             */
-/*   Updated: 2023/05/30 14:42:00 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/06/05 09:35:11 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-// void	setup_eating_tasks(t_diner *diner)
-// {
-// 	diner->shared->eating_tasks[4] = NULL;
-// 	diner->shared->eating_tasks[0] = pick_up_forks;
-// 	diner->shared->eating_tasks[1] = eat_meal;
-// 	diner->shared->eating_tasks[2] = drop_forks;
-// 	diner->shared->eating_tasks[3] = check_if_full;
-// }
 
 int	setup_prog_mutexes(t_diner *diner)
 {
@@ -39,8 +30,6 @@ int	setup_program(t_diner *diner, t_shared *shared)
 	if (setup_prog_mutexes(diner) == ERROR)
 		return (print_error("mutex intialization failed"));
 	diner->shared->full_phils = 0;
-	// setup_eating_tasks(diner);
-	// printf("SHARED before phil_factory nb_philo: %i, time_to_die: %i, time_to_eat: %i, time_to_sleep: %i \n", shared->nb_philo, shared->time_to_die, shared->time_to_eat, shared->time_to_sleep);//
 	if (phil_factory(diner) == ERROR)
 	{
 		destroy_prog_mutexes(diner);
